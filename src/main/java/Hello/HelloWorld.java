e81648ace16a6fef7ec80e8c4985e61a05da18ef
Quentin81 <quentin.aubertot@ensta-bretagne.org>
2017-05-18 13:13:36 +0200

    Add hello world

---
+++ src/main/java/Hello/HelloWorld.java
@@ -0,0 +1,13 @@
+package Hello;
+
+/**
+ * Created by Quentin on 18/05/2017.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
