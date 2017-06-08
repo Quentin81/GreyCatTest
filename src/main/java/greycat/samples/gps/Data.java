42ee7c9bea04d75d6fe2b4c627bfb7a2dd16e604
Quentin81 <quentin.aubertot@ensta-bretagne.org>
2017-06-08 10:26:02 +0200

    Index vehicle by type

--- src/main/java/greycat/samples/gps/Data.java
+++ src/main/java/greycat/samples/gps/Data.java
@@ -31,6 +31,8 @@ public class Data {
 
             String vehicle_array[] = {"vehicle1", "vehicle2", "vehicle3" , "vehicle4", "vehicle5", "vehicle6"};
 
+            String type_vehicle_array[] = {"Plane", "Car", "Train", "Plane", "Car", "Train"};
+
             Double gps_array_vehicle[][] = {{0.0, 0.0, 5.0 , 5.0},{1.5, 0.0, 2.0 , 3.0},{0.0, 1.5, 4.0 , 1.0},{5.0, 2.0, 4.5 , 3.0},{3.0, 3.5, 0.0 , 0.0},{1.0, 2.0, 3.0 , 4.5}};
 
             int timepoint_array[] ={0,1,2,3,4,5,6};  //the timestamp is a long and represents the time concept
@@ -42,13 +44,14 @@ public class Data {
                 data_gps.set("latitude", Type.DOUBLE, gps_array_vehicle[i][1]); //set the value of the sensor
 
                 Node vehicle = g.newNode(0, timepoint_array[0]); //the second param is the time
-                vehicle.set("type", Type.STRING, "vehicle");
+                vehicle.set("type", Type.STRING, type_vehicle_array[i]);
                 vehicle.set("name", Type.STRING, vehicle_array[i]);
 
                 vehicle.addToRelation("has_gps", data_gps); //add the sensor0 to the relation sensors of room0
 
                 System.out.println(vehicle.toString());
                 System.out.println(data_gps.toString());
+
 
                 data_gps.travelInTime(timepoint_array[i+1], (Node data_gps_Ti) -> {
 
@@ -58,7 +61,24 @@ public class Data {
                     j=j+1;
                     System.out.println(data_gps_Ti.toString());
                 });
+
+
+                g.index(0,0,"vehicles", vehicleIndex -> {
+                    vehicleIndex.addToIndex(vehicle, "type");
+
+                    vehicleIndex.find(vehicles -> {
+                        System.out.println("found: "+vehicles.length+" node!");
+                        for (Node vehi : vehicles) {
+                            System.out.println(vehi);
+                        }
+                    },"type","Train");
+                });
             }
         });
+
+        g.disconnect(result -> {
+            System.out.println("Goodbye !");
+        });
+
     }
 }
